3a3372e71065a7f7fd4f7a3528ee1aafa264b077
Anthony Devine <antdevine@hotmail.co.uk>
2020-10-14 21:25:30 +0100

    fonts and styling and new image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Image from "../components/image"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <p className="first">Hello my name is</p>
+    <h1>Anthony Devine.</h1>
+    <h2>I am a Frontend Developer with over 15 years experience in many areas of Web Development.</h2>
+    <p>Visit my Linkedin to connect</p>
+    <a href="https://www.linkedin.com/in/anthony-devine-5bb92745/" target="_blank" rel="noreferrer"><Image/></a> <br />
   </Layout>
 )
 
